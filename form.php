a6e0a1726c60809e81a80cbf85d8ae9f884bd34b
Daniel Balbino <134506811+D-Balbino@users.noreply.github.com>
2023-12-06 08:05:22 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+    if(empty(&$_POST['name']) && empty($_POST['email']){
+        echo "</br carregando...";
+
+    }else{ 
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        echo ('Seu nome é:' . $name . '</br');
+        echo ('Seu email é:' . $email . '</br');
+    }
+    
+    
+    
+    
+    
+?>
